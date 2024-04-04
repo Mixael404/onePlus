e4fb3d7b947049116b661040d4facbe21d6015e3
Mihael404 <slutskij.m.a@gmail.com>
2024-04-04 21:49:31 +0300

    Refactoring. Fix of validation of birthday. Button validation according to array of errors

--- js/currencies.js
+++ js/currencies.js
@@ -7208,7 +7208,7 @@ const data = [
 
 const tableBody = document.querySelector('tbody')
 async function getCurrencies(){
-    const response = await fetch('https://fanated.com/coingecko_markets.json')
+    const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1')
     const data = await response.json()
     console.log(data);
     
--- js/form.js
+++ js/form.js
@@ -4,20 +4,9 @@ const button = form.querySelector("button");
 
 let errors = [];
 
-function addListener(element, mask) {
-  element.addEventListener("blur", (e) => {
-    const value = e.target.value;
-    if (value.match(mask)) {
-      console.log("Valid");
-    } else {
-      console.log("Invalid");
-    }
-  });
-}
-
 function validateBtn() {
   const inputs = Array.from(form.querySelectorAll("input"));
-  if (inputs.some((el) => el.value === "") || errorsWrapper.children.length) {
+  if (inputs.some((el) => el.value === "") || errors.length) {
     button.disabled = true;
   } else {
     button.disabled = false;
@@ -31,18 +20,24 @@ function createError(className, text) {
   errors.push(error);
 }
 
-function resetCurrentErrors(currentErrors){
-    errorsWrapper.innerHTML = ''
-    const resetErrors = errors.map((error) => {
-        if(currentErrors.includes(error.className)){
-            error = null
-        }
-        return error
-    })
-    return filteredErrors = resetErrors.filter(error => error !== null)
+function resetCurrentErrors(currentErrors) {
+  errorsWrapper.innerHTML = "";
+  const resetErrors = errors.map((error) => {
+    if (currentErrors.includes(error.className)) {
+      error = null;
+    }
+    return error;
+  });
+  return (filteredErrors = resetErrors.filter((error) => error !== null));
 }
 
-function validateName() {
+function renderErrors() {
+  errors.forEach((error) => {
+    errorsWrapper.append(error);
+  });
+}
+
+(function validateName() {
   // Только латиница и кириллица. От 4 до 20 символов
   const validMask = /^[а-яёА-ЯЁa-zA-Z]{4,20}$/;
   const name = form.querySelector('[name="name"]');
@@ -51,20 +46,9 @@ function validateName() {
 
   name.addEventListener("blur", (e) => {
     const value = e.target.value;
-    
-    // const resetErrors = errors.map((error) => {
-    //     if(error.classList.contains('illegalName') || error.classList.contains('shortName') || error.classList.contains('longName')){
-    //         error = null
-    //     }
-    //     return error
-    // })
-    errors = resetCurrentErrors(['illegalName','shortName','longName']) 
-    // resetErrors.filter(error => error !== null)
-    // errorsWrapper.innerHTML = ''
 
-    if (value.match(validMask)) {
-      console.log("Valid");
-    } else {
+    errors = resetCurrentErrors(["illegalName", "shortName", "longName"]);
+    if (!value.match(validMask)) {
       if (value.length < 4) {
         createError("shortName", "Слишком короткое имя");
       }
@@ -73,23 +57,21 @@ function validateName() {
         createError("longName", "Слишком длинное имя");
       }
 
-      if(value.match(isNumber)){
+      if (value.match(isNumber)) {
         createError("illegalName", "Имя содержит цифры");
       }
 
-      if(value.match(isSymbol)){
+      if (value.match(isSymbol)) {
         createError("illegalName", "Имя содержит символы");
       }
     }
-    errors.forEach(error => {
-        errorsWrapper.append(error)
-    })
+    renderErrors();
 
     validateBtn();
   });
-}
+})();
 
-function validateSurname() {
+(function validateSurname() {
   // Только латиница и кириллица. От 4 до 20 символов
   const validMask = /^[а-яёА-ЯЁa-zA-Z]{4,20}$/;
   const surname = form.querySelector('[name="surname"]');
@@ -98,40 +80,33 @@ function validateSurname() {
 
   surname.addEventListener("blur", (e) => {
     const value = e.target.value;
-    const resetErrors = errors.map((error) => {
-        if(error.classList.contains('digitsSurName') || error.classList.contains('shortSurName') || error.classList.contains('longSurName') || error.classList.contains('symbolsSurName')){
-            error = null
-        }
-        return error
-    })
-    errors = resetErrors.filter(error => error !== null)
-    
-    errorsWrapper.innerHTML = ''
+    errors = resetCurrentErrors([
+      "digitsSurName",
+      "shortSurName",
+      "longSurName",
+      "symbolsSurName",
+    ]);
 
-    if (value.match(validMask)) {
-      console.log("Valid");
-    } else {
+    if (!value.match(validMask)) {
       if (value.length < 4) {
         createError("shortSurName", "Слишком короткая фамилия");
       }
       if (value.length > 20) {
         createError("longSurName", "Слишком длинная фамилия");
       }
-      if(value.match(isNumber)){
+      if (value.match(isNumber)) {
         createError("digitsSurName", "Фамилия содержит цифры");
       }
-      if(value.match(isSymbol)){
+      if (value.match(isSymbol)) {
         createError("symbolsSurName", "Фамилия содержит символы");
       }
     }
-    errors.forEach(error => {
-        errorsWrapper.append(error)
-    })
+    renderErrors();
     validateBtn();
   });
-}
+})();
 
-function validateEmail() {
+(function validateEmail() {
   // email адрес. Символы, знак @, символы, точка и маленькие латинские буквы
   const validMask =
     /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z](?:[a-z]*[a-z])?/;
@@ -139,29 +114,17 @@ function validateEmail() {
 
   emailInput.addEventListener("blur", (e) => {
     const value = e.target.value;
-    const resetErrors = errors.map((error) => {
-        if(error.classList.contains('illegalEmail')){
-            error = null
-        }
-        return error
-    })
-    errors = resetErrors.filter(error => error !== null)
-    
-    errorsWrapper.innerHTML = ''
+    errors = resetCurrentErrors(["illegalEmail"]);
 
-    if (value.match(validMask)) {
-      console.log("Valid");
-    } else {
-        createError('illegalEmail', 'Введите существующий email адрес')
+    if (!value.match(validMask)) {
+        createError("illegalEmail", "Введите существующий email адрес");
     }
-    errors.forEach(error => {
-        errorsWrapper.append(error)
-    })
+    renderErrors();
     validateBtn();
   });
-}
+})();
 
-function validatePassword() {
+(function validatePassword() {
   // Латинские буквы, цифры и символы. хотя бы одна цифра, символ, заглавная и строчная буквы.
   const validMask =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[a-zA-Z0-9@$!%*#?&]{8,}$/;
@@ -173,96 +136,69 @@ function validatePassword() {
 
   passwordInput.addEventListener("blur", (e) => {
     const value = e.target.value;
-    
-    const resetErrors = errors.map((error) => {
-        const isError = error.classList.contains('shortPassword') || error.classList.contains('noNumber') || error.classList.contains('noUpperCase') || error.classList.contains('noLowerCase') || error.classList.contains('noSymbol')
 
-        if(isError){
-            error = null
-        }
-        return error
-    })
-    errors = resetErrors.filter(error => error !== null)
-    
-    errorsWrapper.innerHTML = ''
+    errors = resetCurrentErrors([
+      "shortPassword",
+      "noNumber",
+      "noUpperCase",
+      "noLowerCase",
+      "noSymbol",
+    ]);
 
-    if(value.match(validMask)){
-        console.log("Valid");
-    } else{
-        if(value.length < 8) createError('shortPassword', 'Слишком короткий пароль')
-        if (!value.match(isNumber)) createError('noNumber', 'Пароль должен содержать хотя бы одну цифру')
-        if (!value.match(isUppercase)) createError('noUpperCase', 'Пароль должен содержать хотя бы одну заглавную букву')
-        if (!value.match(isLowercase)) createError('noLowerCase', 'Пароль должен содержать хотя бы одну строчную букву')
-        if (!value.match(isSymbol)) createError('noSymbol', 'Пароль должен содержать хотя бы один символ')
+    if (!value.match(validMask)) {
+      if (value.length < 8)
+        createError("shortPassword", "Слишком короткий пароль");
+      if (!value.match(isNumber))
+        createError("noNumber", "Пароль должен содержать хотя бы одну цифру");
+      if (!value.match(isUppercase))
+        createError(
+          "noUpperCase",
+          "Пароль должен содержать хотя бы одну заглавную букву"
+        );
+      if (!value.match(isLowercase))
+        createError(
+          "noLowerCase",
+          "Пароль должен содержать хотя бы одну строчную букву"
+        );
+      if (!value.match(isSymbol))
+        createError("noSymbol", "Пароль должен содержать хотя бы один символ");
     }
-    errors.forEach(error => {
-        errorsWrapper.append(error)
-    })
+    renderErrors();
     validateBtn();
   });
-}
+})();
 
-function validateRepetition() {
+(function validateRepetition() {
   const passwordSubmition = form.querySelector('[name="repeat-password"]');
   const passwordInput = form.querySelector('[name="password"]');
 
   passwordSubmition.addEventListener("blur", (e) => {
+    errors = resetCurrentErrors(["notSamePasswsords"]);
 
-    const resetErrors = errors.map((error) => {
-        if(error.classList.contains('notSamePasswsords')){
-            error = null
-        }
-        return error
-    })
-    errors = resetErrors.filter(error => error !== null)
-    
-    errorsWrapper.innerHTML = ''
-
-    if (e.target.value === passwordInput.value) {
-      console.log("Valid");
-    } else {
-      createError("notSamePasswsords", 'Пароли не совпадают')
+    if (e.target.value !== passwordInput.value) {
+      createError("notSamePasswsords", "Пароли не совпадают");
     }
-    errors.forEach(error => {
-        errorsWrapper.append(error)
-    })
+    renderErrors();
     validateBtn();
   });
-}
+})();
 
-function validateBirthday() {
+(function validateBirthday() {
   const birthdayInput = form.querySelector('[name="birthday"]');
 
   birthdayInput.addEventListener("blur", (e) => {
-
-    const resetErrors = errors.map((error) => {
-        if(error.classList.contains('lessAge')){
-            error = null
-        }
-        return error
-    })
-    errors = resetErrors.filter(error => error !== null)
-    
-    errorsWrapper.innerHTML = ''
+    errors = resetCurrentErrors(["lessAge"]);
 
     const birthDay = new Date(e.target.value);
-    const now = Date.now();
-    const age = (now - birthDay) / (365 * 24 * 60 * 60 * 1000);
-    if (age >= 18) {
-      console.log("Valid");
-    } else {
-    createError("lessAge", "Доступно только с 18-ти лет")
+    const now = new Date();
+
+    const eighteenYearsAgo = new Date(now.getFullYear() - 18, now.getMonth(), now.getDate() + 1);
+    if (eighteenYearsAgo < birthDay ) {
+      createError("lessAge", "Доступно только с 18-ти лет");
     }
-    errors.forEach(error => {
-        errorsWrapper.append(error)
-    })
+
+    renderErrors();
     validateBtn();
   });
-}
+})();
 
-validateName();
-validateSurname();
-validateEmail();
-validatePassword();
-validateRepetition();
-validateBirthday();
